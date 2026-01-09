8a7c37d93de5c579e7da5618c6df249073f3e6e0
vivekin <vivek6694@gmail.com>
2026-01-09 09:38:55 +0530

    image cleanup

--- inacle/lib/constants/images.dart
+++ inacle/lib/constants/images.dart
@@ -1,9 +1,4 @@
 class Images {
-  static const String logo = 'assets/images/logo2.png';
-
-  static const String bg = 'assets/images/loginbg.png';
-
+  static const String logo = 'assets/images/logo.png';
   static const String searchIcon = 'assets/images/search.png';
-
-  static const String logo_new = 'assets/images/new_logo.jpeg';
 }
